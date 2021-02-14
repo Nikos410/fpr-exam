ba5f3d18770a429523f956167483bc2e1284c954
Nikos Epping <nikos.epping@web.de>
2021-02-14 22:45:13 +0100

    ElectricPassengerCar: Add JavaDoc comments

--- src/main/java/de/nikos410/fpr/vehiclemanagement/model/ElectricPassengerCar.java
+++ src/main/java/de/nikos410/fpr/vehiclemanagement/model/ElectricPassengerCar.java
@@ -2,13 +2,26 @@ package de.nikos410.fpr.vehiclemanagement.model;
 
 import java.math.BigDecimal;
 
+/**
+ * An electric passenger car.
+ */
 public class ElectricPassengerCar extends PassengerCar {
     private BigDecimal maximumRange;
 
+    /**
+     * Returns the maximum range of this car.
+     *
+     * @return the maximum range of this car.
+     */
     public BigDecimal getMaximumRange() {
         return maximumRange;
     }
 
+    /**
+     * Sets the maximum range of this car.
+     *
+     * @param maximumRange the maximum range of this car.
+     */
     public void setMaximumRange(BigDecimal maximumRange) {
         this.maximumRange = maximumRange;
     }
