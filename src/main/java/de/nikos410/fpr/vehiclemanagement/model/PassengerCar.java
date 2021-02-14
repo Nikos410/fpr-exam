6daa620aa86a9a898480d065655a0577b1c5254d
Nikos Epping <nikos.epping@web.de>
2021-02-14 22:45:06 +0100

    PassengerCar: Add JavaDoc comments

--- src/main/java/de/nikos410/fpr/vehiclemanagement/model/PassengerCar.java
+++ src/main/java/de/nikos410/fpr/vehiclemanagement/model/PassengerCar.java
@@ -1,12 +1,25 @@
 package de.nikos410.fpr.vehiclemanagement.model;
 
+/**
+ * A passenger car.
+ */
 public class PassengerCar extends Vehicle {
     private int numberOfSeats;
 
+    /**
+     * Returns the number of seats of this vehicle.
+     *
+     * @return the number of seats of this vehicle.
+     */
     public int getNumberOfSeats() {
         return numberOfSeats;
     }
 
+    /**
+     * Sets the number of seats of this vehicle.
+     *
+     * @param numberOfSeats the number of seats of this vehicle.
+     */
     public void setNumberOfSeats(int numberOfSeats) {
         this.numberOfSeats = numberOfSeats;
     }
