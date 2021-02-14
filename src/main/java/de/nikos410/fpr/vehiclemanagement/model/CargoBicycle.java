8cc4f10d71f37ff58428194d08b84a2b3c716707
Nikos Epping <nikos.epping@web.de>
2021-02-14 22:44:58 +0100

    CargoBicycle: Add JavaDoc comments

--- src/main/java/de/nikos410/fpr/vehiclemanagement/model/CargoBicycle.java
+++ src/main/java/de/nikos410/fpr/vehiclemanagement/model/CargoBicycle.java
@@ -2,13 +2,26 @@ package de.nikos410.fpr.vehiclemanagement.model;
 
 import java.math.BigDecimal;
 
+/**
+ * A cargo bicycle.
+ */
 public class CargoBicycle extends Vehicle {
     private BigDecimal cargoVolume;
 
+    /**
+     * Returns the cargo volume of this bike in cubic metres (m^3).
+     *
+     * @return the cargo volume of this bike in cubic metres (m^3).
+     */
     public BigDecimal getCargoVolume() {
         return cargoVolume;
     }
 
+    /**
+     * Sets the cargo volume of this bike in cubic metres (m^3).
+     *
+     * @param cargoVolume the cargo volume of this bike in cubic metres (m^3).
+     */
     public void setCargoVolume(BigDecimal cargoVolume) {
         this.cargoVolume = cargoVolume;
     }
