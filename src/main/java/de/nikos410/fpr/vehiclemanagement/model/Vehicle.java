93d6f00a56a411c882518c35af73de72396b2cd9
Nikos Epping <nikos.epping@web.de>
2021-02-14 22:38:36 +0100

    Vehicle: Add JavaDoc comments

--- src/main/java/de/nikos410/fpr/vehiclemanagement/model/Vehicle.java
+++ src/main/java/de/nikos410/fpr/vehiclemanagement/model/Vehicle.java
@@ -1,21 +1,43 @@
 package de.nikos410.fpr.vehiclemanagement.model;
 
+/**
+ * The base entity which all concrete vehicles should be based on.
+ */
 public abstract class Vehicle extends BaseEntity {
     private String modelNumber;
     private String maximumSpeed;
 
+    /**
+     * Returns the model number of this vehicle.
+     *
+     * @return the model number of this vehicle.
+     */
     public String getModelNumber() {
         return modelNumber;
     }
 
+    /**
+     * Sets the model number of this vehicle.
+     *
+     * @param modelNumber the model number of this vehicle.
+     */
     public void setModelNumber(String modelNumber) {
         this.modelNumber = modelNumber;
     }
 
+    /**
+     * Returns the maximum speed of this vehicle.
+     * @return the maximum speed of this vehicle.
+     */
     public String getMaximumSpeed() {
         return maximumSpeed;
     }
 
+    /**
+     * Set the maximum speed of this vehicle.
+     *
+     * @param maximumSpeed the maximum speed of this vehicle.
+     */
     public void setMaximumSpeed(String maximumSpeed) {
         this.maximumSpeed = maximumSpeed;
     }
