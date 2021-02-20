48ca7ccc76fbb56e49dc1a9a65dc9bd8701dbfec
Nikos Epping <nikos.epping@web.de>
2021-02-20 17:31:06 +0100

    InMemoryVehicleRepository: Add JavaDoc comment to class

--- src/main/java/de/nikos410/fpr/vehiclemanagement/repository/InMemoryVehicleRepository.java
+++ src/main/java/de/nikos410/fpr/vehiclemanagement/repository/InMemoryVehicleRepository.java
@@ -5,6 +5,9 @@ import de.nikos410.fpr.vehiclemanagement.model.Vehicle;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+/**
+ * A basic {@link VehicleRepository} implementation that keeps data in memory and does not persist anything.
+ */
 public class InMemoryVehicleRepository extends InMemoryRepository<Vehicle> implements VehicleRepository {
     @Override
     public Collection<Vehicle> findAllByModelNumber(String modelNumber) {
