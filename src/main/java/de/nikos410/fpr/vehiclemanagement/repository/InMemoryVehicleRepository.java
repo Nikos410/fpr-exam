31b86e6b89e0cb99beeb37beb1d49ece4efe48ba
Nikos Epping <nikos.epping@web.de>
2021-02-20 18:22:51 +0100

    InMemoryVehicleRepository: Explicitly return List from findAllByModelNumber

--- src/main/java/de/nikos410/fpr/vehiclemanagement/repository/InMemoryVehicleRepository.java
+++ src/main/java/de/nikos410/fpr/vehiclemanagement/repository/InMemoryVehicleRepository.java
@@ -3,14 +3,21 @@ package de.nikos410.fpr.vehiclemanagement.repository;
 import de.nikos410.fpr.vehiclemanagement.model.Vehicle;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * A basic {@link VehicleRepository} implementation that keeps data in memory and does not persist anything.
  */
 public class InMemoryVehicleRepository extends InMemoryRepository<Vehicle> implements VehicleRepository {
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return a List containing all matching elements in this repository.
+     */
     @Override
-    public Collection<Vehicle> findAllByModelNumber(String modelNumber) {
+    public List<Vehicle> findAllByModelNumber(String modelNumber) {
         return content.stream()
                 .filter(vehicle -> modelNumber.equals(vehicle.getModelNumber()))
                 .collect(Collectors.toList());
